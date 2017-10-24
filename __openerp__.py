6b024d68bddc358927ed2d83ca03fc90e9d7000c
Manuel Marquez <buzondemam@gmail.com>
2017-10-23 22:43:11 -0400

    [ADD] Adds model pricelist.customer.discount.purchase and views for allows users configurate discounts for customer according to their purchases

---
+++ __openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Humanytek - Manuel Marquez <manuel@humanytek.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+{
+    'name': 'Manager of discounts by customer purchases',
+    'version': '9.0.0.1.0',
+    'category': 'Sales',
+    'author': 'Humanytek',
+    'website': "http://www.humanytek.com",
+    'license': 'AGPL-3',
+    'depends': ['product', ],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/pricelist_customer_discount_purchase_view.xml',
+        #'wizard/pricelist_customer_discount_purchase_view.xml',
+    ],
+    'installable': True,
+    'auto_install': False
+}
